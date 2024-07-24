4fb840ef7bd056a037b190c22fcdd709ff07c505
Priyankshu Sheet <135867558+priyankshusheet@users.noreply.github.com>
2024-07-24 20:39:30 +0530

    Add files via upload

---
+++ WordWhirlApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordWhirlApp.swift
+//  WordWhirl
+//
+//  Created by Priyankshu Sheet on 17/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WordWhirlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
